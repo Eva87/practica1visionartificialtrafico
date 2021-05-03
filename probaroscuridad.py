82bf78c4550168eb2c29a51158f48eb85d4757b9
Eva <eva.1987@gmail.com>
2021-05-03 13:10:38 +0200

    color pruba

--- probaroscuridad.py
+++ probaroscuridad.py
@@ -1,6 +1,9 @@
 
 import cv2
 import numpy as np
+from random import random
+from colorsys import hsv_to_rgb
+from matplotlib import pyplot as plt
 
 # el modulo pyplot de matplotlib (que llamaremos plt) nos permite mostrar imagenes
 import matplotlib.pyplot as plt
@@ -10,7 +13,224 @@ import matplotlib.colors
 #mmmm https://unipython.com/umbralizacion-una-imagen/ aqui quizas aparezca algo
 
 imagenoscura = cv2.imread("./test/00400.jpg",1)
+imagenclara = cv2.imread("./test/00597.jpg",1)
+imagenclara = cv2.imread("./test/00464.jpg",1)
 imagenclara = cv2.imread("./test/00482.jpg",1)
+imagenclarabyn = cv2.imread("./test/00482.jpg", cv2.IMREAD_GRAYSCALE)
+imagenoscurabyn = cv2.imread("./test/00400.jpg", cv2.IMREAD_GRAYSCALE)
+
+
+
+
+img_med = cv2.adaptiveThreshold(imagenclarabyn,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,21,15)
+img_gauss = cv2.adaptiveThreshold(imagenclarabyn,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,21,15)
+plt.imshow(imagenclarabyn,'gray');plt.title("Original");plt.show()
+plt.imshow(img_med,'gray');plt.title("Adaptativo 21 media-15");plt.show()
+plt.imshow(img_gauss,'gray');plt.title("Adaptativo 21 Gausiana-15");plt.show()
+
+
+
+
+cv2.imshow(cv2.circle(imagenclara, None, None, "red"))
+
+
+
+cv2.waitKey(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+img=img_gauss
+a=0
+image2 = np.uint8(255.0 * (img - img.min()) / (img.max() - img.min()))
+print('nivel de brillo máximo = ', img.max())
+print('nivel de brillo mìnimo = ', img.min())
+# umbralización
+# imagen oscura 20 y imagen clara 100
+#if img.max()
+imu = ((imagenclarabyn >100)*255).astype(np.uint8)
+#MSER
+output = np.zeros((imu.shape[0],imu.shape[1],3),dtype=np.uint8)
+mser = cv2.MSER_create(_delta=5,_max_variation=0.5,_max_area=20000)
+polygons = mser.detectRegions(imu)
+for polygon in polygons[0]:
+    colorRGB = hsv_to_rgb(random(),1,1)
+    colorRGB = tuple(int(color*255) for color in colorRGB)
+    output = cv2.fillPoly(output,[polygon],colorRGB)
+
+gray = cv2.cvtColor(imagenclara, cv2.COLOR_BGR2GRAY)  # Convertimos la imagen a escala de grises.
+keypoints=mser.detect(gray,None)
+for keypoint in keypoints:
+    radius = int(0.5 * keypoint.size)
+    x, y = np.int64(keypoint.pt)
+    cv2.circle(imagenclara, (x, y), radius, (0, 255, 255), 2)
+    
+
+
+
+cv2.imshow("imu", imu)
+cv2.imshow("output", output)
+cv2.waitKey(0)
+
+
+'''
+
+
+
+
+
+'''import argparse
+
+max_value = 255
+max_value_H = 360 // 2
+low_H = 0
+low_S = 0
+low_V = 0
+high_H = max_value_H
+high_S = max_value
+high_V = max_value
+window_capture_name = 'Video Capture'
+window_detection_name = 'Object Detection'
+low_H_name = 'Low H'
+low_S_name = 'Low S'
+low_V_name = 'Low V'
+high_H_name = 'High H'
+high_S_name = 'High S'
+high_V_name = 'High V'
+
+
+def on_low_H_thresh_trackbar(val):
+    global low_H
+    global high_H
+    low_H = val
+    low_H = min(high_H - 1, low_H)
+    cv2.setTrackbarPos(low_H_name, window_detection_name, low_H)
+
+
+def on_high_H_thresh_trackbar(val):
+    global low_H
+    global high_H
+    high_H = val
+    high_H = max(high_H, low_H + 1)
+    cv2.setTrackbarPos(high_H_name, window_detection_name, high_H)
+
+
+def on_low_S_thresh_trackbar(val):
+    global low_S
+    global high_S
+    low_S = val
+    low_S = min(high_S - 1, low_S)
+    cv2.setTrackbarPos(low_S_name, window_detection_name, low_S)
+
+
+def on_high_S_thresh_trackbar(val):
+    global low_S
+    global high_S
+    high_S = val
+    high_S = max(high_S, low_S + 1)
+    cv2.setTrackbarPos(high_S_name, window_detection_name, high_S)
+
+
+def on_low_V_thresh_trackbar(val):
+    global low_V
+    global high_V
+    low_V = val
+    low_V = min(high_V - 1, low_V)
+    cv2.setTrackbarPos(low_V_name, window_detection_name, low_V)
+
+
+def on_high_V_thresh_trackbar(val):
+    global low_V
+    global high_V
+    high_V = val
+    high_V = max(high_V, low_V + 1)
+    cv2.setTrackbarPos(high_V_name, window_detection_name, high_V)
+
+
+parser = argparse.ArgumentParser(description='Code for Thresholding Operations using inRange tutorial.')
+parser.add_argument('--camera', help='Camera divide number.', default=0, type=int)
+args = parser.parse_args()
+cap = cv2.VideoCapture(args.camera)
+cv2.namedWindow(window_capture_name)
+cv2.namedWindow(window_detection_name)
+cv2.createTrackbar(low_H_name, window_detection_name, low_H, max_value_H, on_low_H_thresh_trackbar)
+cv2.createTrackbar(high_H_name, window_detection_name, high_H, max_value_H, on_high_H_thresh_trackbar)
+cv2.createTrackbar(low_S_name, window_detection_name, low_S, max_value, on_low_S_thresh_trackbar)
+cv2.createTrackbar(high_S_name, window_detection_name, high_S, max_value, on_high_S_thresh_trackbar)
+cv2.createTrackbar(low_V_name, window_detection_name, low_V, max_value, on_low_V_thresh_trackbar)
+cv2.createTrackbar(high_V_name, window_detection_name, high_V, max_value, on_high_V_thresh_trackbar)
+while True:
+
+    ret, frame = cap.read()
+    if frame is None:
+        break
+    frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    frame_threshold = cv2.inRange(frame_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
+
+    cv2.imshow(window_capture_name, frame)
+    cv2.imshow(window_detection_name, frame_threshold)
+
+    key = cv2.waitKey(30)
+    if key == ord('q') or key == 27:
+        break
+
+
+
+cv2.waitKey(0)
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+histogramaoscuro=cv2.calcHist([imagenoscura],[0], None,[256],[0,256])
+histogramaclaro=cv2.calcHist([imagenclara],[0], None,[256],[0,256])
+
+plt.subplot(2, 2, 1)
+plt.plot(histogramaoscuro, 'gray')
+plt.subplot(2, 2, 3)
+plt.plot(histogramaclaro, 'gray')'''
+
+
+
+
+
+
+
+
+
 
 '''imagenclarahsv=cv2.cvtColor(imagenclara, cv2.COLOR_BGR2HSV)
 hclara,sclara,vclara=cv2.split(imagenclarahsv)
@@ -31,6 +251,9 @@ if cv2.countNonZero(imagenclara) == 0:
 else:
     print ("Colored imagenclara")'''
 
+
+
+'''
 from matplotlib import pyplot as plt
 
 ret, thresh1 = cv2.threshold(imagenclara, 127, 255, cv2.THRESH_BINARY)
@@ -62,6 +285,6 @@ miArray = np.arange(6)
 for i in miArray:
     plt.subplot(2, 3, i + 1), plt.imshow(images[i], 'gray')
     plt.title(titles[i])
-    plt.xticks([]), plt.yticks([])
+    plt.xticks([]), plt.yticks([])'''
 
 plt.show()
